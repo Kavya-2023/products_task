40e91d61dfe48f10e5dc564c292e4360eb3c5573
kavya varanasi <varanasikavya0@gmail.com>
2023-11-02 21:38:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
+import Data from "./components/data";
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState(Data);
+  const [pageSize, setPageSize] = useState(3);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [records, setRecords] = useState(items.slice(0, pageSize));
+  const [pageCount, setPageCount] = useState(Math.ceil(items.length / pageSize));
+  const [searchQuery, setSearchQuery] = useState('');
+  const [order, setOrder] = useState('asc');
+
+  const filterItems = (query) => {
+    const filteredData = records.filter(item =>
+      item.title.toLowerCase().includes(query.toLowerCase())
+    );
+    return filteredData;
+  };
+  const sortItems = (data) => {
+    if (order === "asc") {
+      return data.sort((a, b) => a.price - b.price);
+    } else {
+      return data.sort((a, b) => b.price - a.price);
+    }
+  };
+  const onPageChange = (index) => {
+    setCurrentPage(index);
+    let records = items.slice(index * pageSize, pageSize * (index + 1));
+    setRecords(records);
+  };
+  const onLowToHigh = () => {
+    setOrder('asc')
+  }
+  const onHighToLow = () => {
+    setOrder('desc')
+  }
+  useEffect(() => {
+    let filteredData = filterItems(searchQuery);
+    const sortedData = sortItems(filteredData);
+    setPageCount(Math.ceil(items.length / pageSize));
+    setRecords(sortedData.slice(0, pageSize));
+
+  }, [searchQuery, items, pageSize, order]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="big-container">
+      <nav className="d-flex mt-3">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Search Here.."
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <div className="filter-container d-flex">
+          <button className="filter-btn btn btn-outline-primary" onClick={onLowToHigh}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+            </svg>
+          </button>
+          <button className="filter-btn btn btn-outline-primary" onClick={onHighToLow}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      <div className="App d-flex flex-wrap justify-content-center">
+        {records.map((item) => (
+          <div className="col-4 p-3" key={item.id}>
+            <div className="box">
+              <img src={item.img} alt={item.title} className="image" />
+              <h4 className="card-heading">{item.title}</h4>
+              <p>&#8377;{item.price}</p>
+              <p className="card-desc">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 0} className="btn">Prev</button>
+        {Array(pageCount).fill(null).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => onPageChange(index)}
+            className={`${currentPage === index ? 'btn btn-primary' : 'btn'}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === pageCount - 1}
+          className="btn"
         >
-          Learn React
-        </a>
-      </header>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,131 @@
+import f1 from "../assets/f1.jpg";
+import f2 from "../assets/f2.jpg";
+import f3 from "../assets/f3.jpg";
+import f4 from "../assets/f4.jpg";
+import f5 from "../assets/f5.jpg";
+import f6 from "../assets/f6.jpg";
+import f7 from "../assets/f7.jpg";
+import f8 from "../assets/f8.jpg";
+import n1 from "../assets/n1.jpg";
+import n2 from "../assets/n2.jpg";
+import n3 from "../assets/n3.jpg";
+import n4 from "../assets/n4.jpg";
+import n5 from "../assets/n5.jpg";
+import n6 from "../assets/n6.jpg";
+import n7 from "../assets/n7.jpg";
+import n8 from "../assets/n8.jpg";
+const Data = [
+    {
+        id: 1,
+        img: f1,
+        title: "polo shirt",
+        price: 450,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 2,
+        img: f2,
+        title: "shirt waist",
+        price: 300,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 3,
+        img: f3,
+        title: "cuban collat shirt",
+        price: 250,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 4,
+        img: f4,
+        title: "henely shirt",
+        price: 600,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 5,
+        img: f5,
+        title: "fabric shirt",
+        price: 500,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 6,
+        img: f6,
+        title: "base shirt",
+        price: 450,
+        description: "this is a shirt with base limed fabric"
+    },
+    {
+        id: 7,
+        img: f7,
+        title: "western pant",
+        price: "320",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 8,
+        img: f8,
+        title: "blouse shirt",
+        price: "300",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 9,
+        img: n1,
+        title: "pin tuck",
+        price: "420",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 10,
+        img: n2,
+        title: "ruffle front",
+        price: "450",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 11,
+        img: n3,
+        title: "princess vest",
+        price: "700",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 12,
+        img: n4,
+        title: "peplum",
+        price: "640",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 13,
+        img: n5,
+        title: "tunic",
+        price: "590",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 14,
+        img: n6,
+        title: "smock",
+        price: "670",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 15,
+        img: n7,
+        title: "polo",
+        price: "400",
+        description: "this is designed with different thread",
+    },
+    {
+        id: 16,
+        img: n8,
+        title: "sweatshirt",
+        price: "330",
+        description: "this is designed with different thread",
+    },
+]
+export default Data;
